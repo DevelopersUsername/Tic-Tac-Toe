6b12b6f0e5dd28ec9d40026387d908ed96ed6459
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2020-08-14 00:30:10 +0300

    Init stage 5

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int emptyCells = 11;
+        String startCells = "_________";
+        String currentSymbol = "X";
+
         String[][] cellsMatrix = new String[3][3];
-
-        System.out.print("Enter cells: ");
-
         Scanner scanner = new Scanner(System.in);
-        fillCells(cellsMatrix, scanner.nextLine().split(""));
-        printCells(cellsMatrix);
 
-        System.out.print("Enter the coordinates: ");
-        while (checkCoordinates(cellsMatrix, scanner)) {
+        fillCells(cellsMatrix, startCells.split(""));
+        while (checkWin(cellsMatrix, currentSymbol, --emptyCells)) {
+            printCells(cellsMatrix);
+
             System.out.print("Enter the coordinates: ");
+            while (checkCoordinates(cellsMatrix, scanner, currentSymbol)) {
+                System.out.print("Enter the coordinates: ");
+            }
+            currentSymbol = getCurrentSymbol(currentSymbol);
         }
-
-        printCells(cellsMatrix);
     }
 
     static void fillCells(String[][] cellsMatrix, String[] cellsArray) {
@@ -47,7 +50,7 @@ public class Main {
         System.out.println("---------");
     }
 
-    static boolean checkCoordinates(String[][] cellsMatrix, Scanner scanner) {
+    static boolean checkCoordinates(String[][] cellsMatrix, Scanner scanner, String currentSymbol) {
         int i, j;
 
         while(!scanner.hasNextInt()) {
@@ -65,8 +68,39 @@ public class Main {
             System.out.println("This cell is occupied! Choose another one!");
             return true;
         } else {
-            cellsMatrix[j][i] = "X";
+            cellsMatrix[j][i] = currentSymbol;
             return false;
+        }
+    }
+
+    static boolean checkWin(String[][] cellsMatrix, String currentSymbol, int emptyCells) {
+
+        currentSymbol = getCurrentSymbol(currentSymbol);
+        if ((cellsMatrix[0][0].equals(currentSymbol) && cellsMatrix[0][1].equals(currentSymbol) && cellsMatrix[0][2].equals(currentSymbol))
+                || (cellsMatrix[1][0].equals(currentSymbol) && cellsMatrix[1][1].equals(currentSymbol) && cellsMatrix[1][2].equals(currentSymbol))
+                || (cellsMatrix[2][0].equals(currentSymbol) && cellsMatrix[2][1].equals(currentSymbol) && cellsMatrix[2][2].equals(currentSymbol))
+                || (cellsMatrix[0][0].equals(currentSymbol) && cellsMatrix[1][0].equals(currentSymbol) && cellsMatrix[2][0].equals(currentSymbol))
+                || (cellsMatrix[0][1].equals(currentSymbol) && cellsMatrix[1][1].equals(currentSymbol) && cellsMatrix[2][1].equals(currentSymbol))
+                || (cellsMatrix[0][2].equals(currentSymbol) && cellsMatrix[1][2].equals(currentSymbol) && cellsMatrix[2][2].equals(currentSymbol))
+                || (cellsMatrix[0][0].equals(currentSymbol) && cellsMatrix[1][1].equals(currentSymbol) && cellsMatrix[2][2].equals(currentSymbol))
+                || (cellsMatrix[2][0].equals(currentSymbol) && cellsMatrix[1][1].equals(currentSymbol) && cellsMatrix[0][2].equals(currentSymbol))) {
+
+            printCells(cellsMatrix);
+            System.out.printf("%s wins", currentSymbol);
+            return false;
+        } else if (emptyCells == 0) {
+            System.out.println("Game not finished");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    static String getCurrentSymbol(String currentSymbol) {
+        if ("X".equals(currentSymbol)) {
+            return "O";
+        } else {
+            return "X";
         }
     }
 }
