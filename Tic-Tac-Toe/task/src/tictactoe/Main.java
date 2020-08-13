0d87b12030512aa7d95980c43f261af9c76cc658
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2020-08-13 21:12:01 +0300

    Init stage 4

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -5,73 +5,68 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int index = 0;
-        int emptyCells = 0;
-        int XCells = 0;
-        int OCells = 0;
-        boolean XWins = false;
-        boolean OWins = false;
+        String[][] cellsMatrix = new String[3][3];
 
         System.out.print("Enter cells: ");
 
         Scanner scanner = new Scanner(System.in);
-        String[] cellsArray = scanner.nextLine().split("");
+        fillCells(cellsMatrix, scanner.nextLine().split(""));
+        printCells(cellsMatrix);
 
+        System.out.print("Enter the coordinates: ");
+        while (checkCoordinates(cellsMatrix, scanner)) {
+            System.out.print("Enter the coordinates: ");
+        }
+
+        printCells(cellsMatrix);
+    }
+
+    static void fillCells(String[][] cellsMatrix, String[] cellsArray) {
+        int i = 0;
+        int j = 2;
+
+        for (int k = cellsArray.length - 1; k >= 0; k--) {
+            cellsMatrix[i][j] = cellsArray[k];
+
+            if (--j < 0) {
+                j = 2;
+                i++;
+            }
+        }
+    }
+
+    static void printCells(String[][] cellsMatrix) {
         System.out.println("---------");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 2; i >= 0; i--) {
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                System.out.printf("%s ", cellsArray[index]);
-                switch (cellsArray[index]) {
-                    case " ":
-                    case "_":
-                        emptyCells++;
-                        break;
-                    case "X":
-                        XCells++;
-                        break;
-                    case "O":
-                        OCells++;
-                        break;
-                }
-                index++;
+                System.out.printf("%s ", cellsMatrix[i][j]);
             }
             System.out.println("|");
         }
-
-        boolean Impossible = (Math.abs(XCells - OCells) > 1);
-
-        if ((cellsArray[0].equals("X") && cellsArray[1].equals("X") && cellsArray[2].equals("X")) ||
-                (cellsArray[1].equals("X") && cellsArray[4].equals("X") && cellsArray[7].equals("X")) ||
-                (cellsArray[0].equals("X") && cellsArray[3].equals("X") && cellsArray[6].equals("X")) ||
-                (cellsArray[0].equals("X") && cellsArray[4].equals("X") && cellsArray[8].equals("X")) ||
-                (cellsArray[2].equals("X") && cellsArray[4].equals("X") && cellsArray[6].equals("X")) ||
-                (cellsArray[2].equals("X") && cellsArray[5].equals("X") && cellsArray[8].equals("X")) ||
-                (cellsArray[6].equals("X") && cellsArray[7].equals("X") && cellsArray[8].equals("X"))) {
-            XWins = true;
-        }
-        if ((cellsArray[0].equals("O") && cellsArray[1].equals("O") && cellsArray[2].equals("O")) ||
-                (cellsArray[1].equals("O") && cellsArray[4].equals("O") && cellsArray[7].equals("O")) ||
-                (cellsArray[0].equals("O") && cellsArray[3].equals("O") && cellsArray[6].equals("O")) ||
-                (cellsArray[0].equals("O") && cellsArray[4].equals("O") && cellsArray[8].equals("O")) ||
-                (cellsArray[2].equals("O") && cellsArray[4].equals("O") && cellsArray[6].equals("O")) ||
-                (cellsArray[2].equals("O") && cellsArray[5].equals("O") && cellsArray[8].equals("O")) ||
-                (cellsArray[6].equals("O") && cellsArray[7].equals("O") && cellsArray[8].equals("O"))) {
-            OWins = true;
-        }
-
         System.out.println("---------");
+    }
 
-        if (emptyCells > 0 && !Impossible && !XWins && !OWins) {
-            System.out.println("Game not finished");
-        } else if (!XWins && !OWins && !Impossible) {
-            System.out.println("Draw");
-        } else if (XWins && !OWins && !Impossible) {
-            System.out.println("X wins");
-        } else if (OWins && !XWins && !Impossible) {
-            System.out.println("O wins");
+    static boolean checkCoordinates(String[][] cellsMatrix, Scanner scanner) {
+        int i, j;
+
+        while(!scanner.hasNextInt()) {
+            System.out.println("You should enter numbers!");
+            System.out.print("Enter the coordinates: ");
+            scanner.next();
+        }
+        i = scanner.nextInt();
+        j = scanner.nextInt();
+
+        if (--i > 2 || --j > 2) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return true;
+        } else if (cellsMatrix[j][i].equals("X") || cellsMatrix[j][i].equals("O")) {
+            System.out.println("This cell is occupied! Choose another one!");
+            return true;
         } else {
-            System.out.println("Impossible");
+            cellsMatrix[j][i] = "X";
+            return false;
         }
     }
 }
